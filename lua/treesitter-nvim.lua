3fafb563915aa54e2011ec5ef800116ea3f72a91
k <mol.tired@gmail.com>
2021-06-23 03:41:53 -0500

    treesitter-nvim: enable indent

--- lua/treesitter-nvim.lua
+++ lua/treesitter-nvim.lua
@@ -1,21 +1,25 @@
-local tsr = require 'nvim-treesitter.configs'
+local ts_config = require 'nvim-treesitter.configs'
 
-tsr.setup {
-    ensure_installed = {
-        "go",
-        -- "c",
-        -- "c++",
-        -- "rust",
-        -- "javascript",
-        -- "typescript",
-        "bash",
-        "lua",
-        -- "html",
-        -- "sass",
-        -- "json",
-    },
-    highlight = {
-        enable = true,
-        use_languagetree = true
-    }
+ts_config.setup {
+	ensure_installed = {
+		"go",
+		-- "c",
+		-- "cpp",
+		-- "rust",
+		-- "javascript",
+		-- "typescript",
+		-- "tsx",
+		"bash",
+		"lua",
+		-- "html",
+		-- "sass",
+		-- "json",
+	},
+	highlight = {
+		enable = true,
+		use_languagetree = true
+	},
+	indent = {
+		enable = true,
+	},
 }
