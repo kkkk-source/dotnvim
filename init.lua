21a9e9bdd7444d9dfb03ebcc1d59622dbcdf10d4
k <mol.tired@gmail.com>
2021-06-23 03:42:30 -0500

    init.lua: migrate opts from init.vim

--- init.lua
+++ init.lua
@@ -1,30 +1,35 @@
-require 'pluginList'
-require 'treesitter-nvim'
+require('pluginList')
+require('treesitter-nvim')
+require('indent-blankline')
 
-vim.o.hidden = true -- enable background buffers
-vim.o.list = true   -- show some invisible characters
-vim.o.scrolloff = 8
+-- global
+vim.o.hidden = true
 vim.o.incsearch = true
-
-vim.wo.number = true -- show line numbers
-vim.wo.relativenumber = true -- relative line numbers
-
---[[
-set noswapfile
-set nohlsearch
-set nobackup
-set nowritebackup
-set relativenumber
-set undodir=~/.config/nvim/undo
-set undofile
-set updatetime=300
-set signcolumn=yes
-set grepprg=ag\ --vimgrep
-set expandtab
-set tabstop=2
-set softtabstop=2
-set shiftwidth=2
-set clipboard=unnamedplus
-set path+=**
-set wildmenu
-]]
+vim.o.listchars = 'tab:▶ ,eol:¬,trail:⋅,extends:❯,precedes:❮'
+vim.o.scrolloff = 7
+vim.o.updatetime = 500
+vim.o.wildmode = 'longest,full'
+vim.o.wildignore = '*.o,*~'
+vim.o.laststatus = 2
+vim.o.showmode = false
+vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.o.completeopt = 'menuone,noselect'
+vim.o.display = 'msgsep'
+vim.o.mouse = 'nivh'
+vim.o.termguicolors = true
+vim.o.background = 'dark'
+-- window
+vim.wo.list = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.cursorline = true
+vim.wo.signcolumn = 'yes:1'
+-- buffer
+vim.bo.undofile = true
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 0
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.smartindent = true
+vim.bo.modeline = false
+vim.bo.expandtab = true
