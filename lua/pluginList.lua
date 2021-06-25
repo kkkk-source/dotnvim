92662522d4f99afb9f7194e0a24ced1506adbba3
k <mol.tired@gmail.com>
2021-06-24 19:40:39 -0500

    pluginList: add git related plugins

--- lua/pluginList.lua
+++ lua/pluginList.lua
@@ -9,8 +9,34 @@ return packer.startup(
 		use 'nvim-treesitter/nvim-treesitter'
 
 		use {
-			"lukas-reineke/indent-blankline.nvim",
+			'lukas-reineke/indent-blankline.nvim',
 			branch = "lua"
+		}
+
+		use {
+			'junegunn/fzf.vim',
+			requires = {
+				{
+					'junegunn/fzf', 
+					run = function() vim.fn['fzf#install']() end
+				}
+			}
+		}
+
+		use {
+			'kyazdani42/nvim-tree.lua',
+			requires = {'kyazdani42/nvim-web-devicons'}
+		}
+
+
+		use 'tpope/vim-fugitive'
+
+		use {
+			'lewis6991/gitsigns.nvim',
+			requires = {
+				'nvim-lua/plenary.nvim'
+			},
+			config = function() require('gitsigns').setup() end
 		}
 	end
 )
