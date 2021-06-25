a12ba9a4764d9064238d3f717f3bad5beab64912
k <mol.tired@gmail.com>
2021-06-24 19:41:08 -0500

    init: update

--- init.lua
+++ init.lua
@@ -2,10 +2,16 @@ require('pluginList')
 require('treesitter-nvim')
 require('indent-blankline')
 
+vim.g.mapleader = ' '
+
+require('fzf-vim')
+require('nvimTree')
+require('fugitive')
+
 -- global
 vim.o.hidden = true
 vim.o.incsearch = true
-vim.o.listchars = 'tab:▶ ,eol:¬,trail:⋅,extends:❯,precedes:❮'
+vim.o.listchars = 'tab:▸\\ ,eol:¬,trail:⋅,extends:❯,precedes:❮'
 vim.o.scrolloff = 7
 vim.o.updatetime = 500
 vim.o.wildmode = 'longest,full'
@@ -16,14 +22,15 @@ vim.o.shortmess = vim.o.shortmess .. 'c'
 vim.o.completeopt = 'menuone,noselect'
 vim.o.display = 'msgsep'
 vim.o.mouse = 'nivh'
-vim.o.termguicolors = true
-vim.o.background = 'dark'
+-- using vim.o.termguicolors = true is not working properly
+
 -- window
 vim.wo.list = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.cursorline = true
 vim.wo.signcolumn = 'yes:1'
+
 -- buffer
 vim.bo.undofile = true
 vim.bo.tabstop = 2
@@ -33,3 +40,18 @@ vim.bo.shiftwidth = 2
 vim.bo.smartindent = true
 vim.bo.modeline = false
 vim.bo.expandtab = true
+
+-- mappings
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>bp',
+  [[<Cmd>tabnew<CR><Cmd>term<CR>]],
+  {
+    noremap = true,
+    silent = true
+  }
+)
+
+-- highlights
+vim.cmd([[ highlight VertSplit cterm=NONE ]])
+vim.cmd([[ highlight SignColumn ctermbg=NONE ctermfg=NONE ]])
